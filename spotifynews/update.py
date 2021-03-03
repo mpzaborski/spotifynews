c10f0025570346bd9028ad70f5b26ed2e22d7f9a
Mateusz Zaborski <mateusz.zaborski@assaabloy.com>
2021-03-04 00:03:35 +0100

    Added support for longer playlists

--- spotifynews/update.py
+++ spotifynews/update.py
@@ -1,5 +1,5 @@
 from .database.sqlite3db import Sqlite3db
-from .spotify.client import Client
+from .spotify.client import Client, chunk_playlist
 
 
 def news(database_f, original_playlist_id):
@@ -9,10 +9,8 @@ def news(database_f, original_playlist_id):
     mirror_playlist_item_ids = client.update_mirror_playlist(original_playlist_id=original_playlist_id,
                                                              known_track_ids=known_track_ids,
                                                              playlist_part_name='news')
-    if not mirror_playlist_item_ids:
-        return False
     db.insert_songs(playlist_id=original_playlist_id, song_ids=mirror_playlist_item_ids)
-    return True
+    return mirror_playlist_item_ids
 
 
 def collections(database_f, original_playlist_id):
@@ -22,4 +20,7 @@ def collections(database_f, original_playlist_id):
     playlist_id = client.get_mirror_playlist_id(original_playlist_id=original_playlist_id,
                                                 playlist_part_name='complete')
     client.playlist_remove_all_items(playlist_id=playlist_id)
-    client.playlist_add_items(playlist_id=playlist_id, items=known_track_ids)
+
+    for chunk in chunk_playlist(known_track_ids):
+        client.playlist_add_items(playlist_id=playlist_id, items=chunk)
+    return known_track_ids
--- tests/test_update.py
+++ tests/test_update.py
@@ -1,6 +1,6 @@
 from spotifynews.update import news, collections
 
-sample_playlist_id = 'spotify:playlist:37i9dQZF1DX0XUsuxWHRQd'
+sample_playlist_id = 'spotify:playlist:5FmmxErJczcrEwIFGIviYo'
 
 
 def test_news(tmp_path):
@@ -10,4 +10,4 @@ def test_news(tmp_path):
 
 def test_collections(tmp_path):
     assert news(database_f=tmp_path / "test.db", original_playlist_id=sample_playlist_id)
-    collections(database_f=tmp_path / "test.db", original_playlist_id=sample_playlist_id)
+    assert collections(database_f=tmp_path / "test.db", original_playlist_id=sample_playlist_id)
